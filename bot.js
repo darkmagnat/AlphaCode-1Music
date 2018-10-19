b47ce423cbe4de8001ce9bd2cb76c86c4b654ca8
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-19 16:47:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | By : LooZ`,"http://twitch.tv/S-F")
+client.user.setGame(`آلَآيّّآمِـ تًُـمِـر وُ آلَسًسًـنٌيّّنٌ قهِوُة |By : LooZ`,"http://twitch.tv/S-F")
 client.user.setStatus("online")
  
 });
